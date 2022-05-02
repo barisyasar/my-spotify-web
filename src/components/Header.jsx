485a73628689380af29e50ec941d21ec5779904f
Barış Yaşar <baris.yasar@altima.com.tr>
2022-05-03 00:06:48 +0300

    updated

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <TextField id="standard-basic" label="Search" size='small' variant="standard" />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
